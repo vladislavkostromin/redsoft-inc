7de5b0a3c37a0d7ac8faf575e762c5a14f91fe9b
vladislavkostromin <61909342+vladislavkostromin@users.noreply.github.com>
2021-01-15 02:12:36 +0500

    get запрос

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 
+
+
+  fetch('https://reqres.in/api/products/3')
+    .then(data => {
+      console.log(data)
+      return data.text();
+    })
+
+    .then(data => {
+      console.log(data)
+    });
+
+
+  
+    
+    
 
 });
