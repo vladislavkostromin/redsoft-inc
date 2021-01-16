8307aa1fb5bd1fc39fd195627c189377b7480738
vladislavkostromin <61909342+vladislavkostromin@users.noreply.github.com>
2021-01-17 01:55:50 +0500

    Нажатие кнопки в localStorage

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
 
   const getMessage = document.querySelector('.btn-buy');
+  const getBasket = document.querySelector('.btn--basket');
+
 
   getMessage.addEventListener('click', () => {
     fetch('https://reqres.in/api/products/3')
@@ -15,4 +17,27 @@ document.addEventListener("DOMContentLoaded", function () {
 
   });
 
+
+  document.querySelector(".btn-buy") ?.addEventListener("click", () =>
+    localStorage.setItem("Товар в корзине", "Товар в корзине"));
+
+  getMessage.addEventListener('click', () => {
+    if (getMessage) {
+      getBasket.style.display = 'flex'
+      getMessage.style.display = 'none'
+    }
+
+  });
+
+  if (localStorage.getItem("Товар в корзине")) {
+    getMessage.style.display = 'none'
+    getBasket.style.display = 'flex'
+  } else if (localStorage.getItem(!"Товар в корзине")) {
+    getMessage.style.display = 'flex'
+    getBasket.style.display = 'none'
+  }
+
 });
+
+
+
